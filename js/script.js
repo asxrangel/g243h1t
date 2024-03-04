6e8058c2f2585db9687489e2a8fb564dea07eb65
Marianna Mika <110108982+asxrangel@users.noreply.github.com>
2024-03-04 12:29:28 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 imprime = (msg) => console.log(msg);
+
+fazerSorteio = ()  => {
+    let num1 = document.getElementById("inicio").value;
+    let num2 = document.getElementById("fim").value;
+    num2 = Number(num2) + 1;
+    let sorteio = Math.floor(Math.random() * Number(num2) - Number(num1));
+
+    document.getElementById("resultado").innerHTML = sorteio;   
+}
