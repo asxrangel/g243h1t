559c20f7ad7685eff809250545430b0c7deb441a
Marianna Mika <110108982+asxrangel@users.noreply.github.com>
2024-03-11 12:34:34 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,23 +11,29 @@ fazerSorteio = ()  => {
 function escolher_aba(btn){
     for(let i = 1; i <= 4; i++){
         let id_btn = "btn"+i;
+        let id_aba = "aba"+i;
         document.getElementById(id_btn).style.backgroundColor = "aqua";
         document.getElementById(id_btn).disabled = false;
+        document.getElementsById(id_aba).style.display = "none";
     }
     if(btn == "1"){
-        document.getElementById("btn1").style.backgroundColor = "green";
+        document.getElementById("btn1").style.backgroundColor = "aquamarine";
         document.getElementById("btn1").disabled = true;
+        document.getElementById("ab1").style.display = "block";
     }
     if(btn == "2"){
-        document.getElementById("btn2").style.backgroundColor = "green";
+        document.getElementById("btn2").style.backgroundColor = "aquamarine";
         document.getElementById("btn2").disabled = true;
+        document.getElementById("ab2").style.display = "block";
     }
     if(btn == "3"){
-        document.getElementById("btn3").style.backgroundColor = "green";
+        document.getElementById("btn3").style.backgroundColor = "aquamarine";
         document.getElementById("btn3").disabled = true;
+        document.getElementById("ab3").style.display = "block";
     }
     if(btn == "4"){
-        document.getElementById("btn4").style.backgroundColor = "green";
+        document.getElementById("btn4").style.backgroundColor = "aquamarine";
         document.getElementById("btn4").disabled = true;
+        document.getElementById("ab4").style.display = "block";
     }
 }
