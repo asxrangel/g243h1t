4cc5932a300749a4981b288a66ae1b14d173ba64
Marianna Mika <110108982+asxrangel@users.noreply.github.com>
2024-03-11 12:13:04 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ fazerSorteio = ()  => {
 
     document.getElementById("resultado").innerHTML = sorteio;   
 }
+function escolher_aba(btn){
+    for(let i = 1; i <= 4; i++){
+        let id_btn = "btn"+i;
+        document.getElementById(id_btn).style.backgroundColor = "aqua";
+        document.getElementById(id_btn).disabled = false;
+    }
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "green";
+        document.getElementById("btn1").disabled = true;
+    }
+    if(btn == "2"){
+        document.getElementById("btn2").style.backgroundColor = "green";
+        document.getElementById("btn2").disabled = true;
+    }
+    if(btn == "3"){
+        document.getElementById("btn3").style.backgroundColor = "green";
+        document.getElementById("btn3").disabled = true;
+    }
+    if(btn == "4"){
+        document.getElementById("btn4").style.backgroundColor = "green";
+        document.getElementById("btn4").disabled = true;
+    }
+}
